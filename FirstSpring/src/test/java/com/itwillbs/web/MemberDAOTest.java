8cd61d060f3fafe66b219baa615f4ff445341f69
itwill07 <blacksugars2s2@gmail.com>
2022-12-01 17:19:23 +0900

    회원정보저장, 특정회원정보 조회, 로그인 처리

--- FirstSpring/src/test/java/com/itwillbs/web/MemberDAOTest.java
+++ FirstSpring/src/test/java/com/itwillbs/web/MemberDAOTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.itwillbs.domain.MemberVO;
 import com.itwillbs.persistence.MemberDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,10 +19,59 @@ public class MemberDAOTest {
 	// DAO 객체 필요함
 	@Inject
 	private MemberDAO dao;
+	// MemberDAO dao = new MemberDAO(); 의미
 	
-	@Test
+	//@Test
 	public void 시간정보조회하기() {
 		System.out.println(dao.getServerTime());
 	}
 
+	
+	// 회원정보저장(Create)
+	//@Test
+	public void createUser() {
+
+		// 회원정보 생성(사용자 입력 정보) // 현재 없는 단계이므로 테스트용으로 만든 상태
+		MemberVO vo = new MemberVO();
+		vo.setUserid("itwill01");
+		vo.setUserpw("1234");
+		vo.setUsername("사용자01");
+		vo.setUseremail("user@user.com");
+		
+		dao.insertMember(vo);
+	}
+	
+	// 특정 회원의 모든정보를 조회
+	//@Test
+	public void getUserInfo() {
+		
+		MemberVO vo = dao.getMember("admin");
+		
+		System.out.println(" Test : " +vo);
+	}
+	
+	// 로그인 처리
+	@Test
+	public void loginMember() {
+		
+		MemberVO vo = dao.loginMember("admin", "1234");
+	
+		if(vo != null) {
+			System.out.println(" 로그인 성공! ");
+		}else {
+			System.out.println(" 로그인 실패!");
+		}
+		
+		MemberVO insertVO = new MemberVO();
+		insertVO.setUserid("admin");
+		insertVO.setUserpw("1234");
+		MemberVO vo2 = dao.loginMember(insertVO);
+		
+		if(vo2 != null) {
+			System.out.println(" 로그인 성공! ");
+		}else {
+			System.out.println(" 로그인 실패!");
+		}
+	}
+	
 }
